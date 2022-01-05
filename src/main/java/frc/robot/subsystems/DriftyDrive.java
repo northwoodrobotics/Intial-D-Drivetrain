7bcd7a1f12f7f97be0031105b4d69f2ac451695e
Kirk <robotics@northwoodschool.com>
2022-01-05 11:50:30 -0500

    start DriftyDrive

--- src/main/java/frc/robot/subsystems/DriftyDrive.java
+++ src/main/java/frc/robot/subsystems/DriftyDrive.java
@@ -1,8 +1,35 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import DriftyLib.LazySparkMax;
+import DriftyLib.SparkMaxFactory;
 
 public class DriftyDrive extends SubsystemBase{
+    private final LazySparkMax mLeftMaster, mRightMaster, mLeftSlave, mRightSlave; 
+
+
+    private void configureSpark(LazySparkMax sparkMax, boolean left, boolean master){
+        sparkMax.setInverted(!left);
+        sparkMax.enableVoltageCompensation(12.0);
+        sparkMax.setClosedLoopRampRate(0.0);
+    }
+
+    private DriftyDrive(){
+        mLeftMaster = SparkMaxFactory.createDefaultSparkMax(1);
+        configureSpark(mLeftMaster, true, true);
+        mLeftSlave = SparkMaxFactory.createPermanentSlaveSparkMax(2, mLeftMaster);
+        configureSpark(mLeftSlave, true, false);
+
+        mRightMaster = SparkMaxFactory.createDefaultSparkMax(3);
+        configureSpark(mRightMaster, false, true);
+
+        mRightSlave = SparkMaxFactory.createPermanentSlaveSparkMax(4, mRightMaster);
+        configureSpark(mRightSlave, false, false);
+    }
+
+    
+
+
 
 
     
